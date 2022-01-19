1f8a695d85b35c23358f111dc14cfa346e030e12
jukangpark <skyxxx9339@naver.com>
2022-01-19 17:53:13 +0900

    start

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  color:black;
+  line-height: 1.2;
+  background:linear-gradient(135deg,#e09,#d0e);
+  }
+  a {
+    text-decoration:none;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
